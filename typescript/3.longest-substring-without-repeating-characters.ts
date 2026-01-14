b25f576a38d69dadecbabb9f904673e2608bbd05
Benjamin Cofrancesco <ben@enxystudio.com>
2026-01-14 21:08:15 +1100

    chore: sliding window with max diff cause im silly

--- typescript/3.longest-substring-without-repeating-characters.ts
+++ typescript/3.longest-substring-without-repeating-characters.ts
@@ -5,23 +5,18 @@
  */
 
 function lengthOfLongestSubstring(s: string): number {
-  let map = new Map<string, number>();
+  let set = new Set();
+  let left = 0;
   let longest = 0;
 
-  const split = s.split("");
-  for (let i = 0; i < split.length; i++) {
-    for (let j = i; j < split.length; j++) {
-      if (map.size > longest) {
-        longest = map.size;
-      }
-
-      if (map.has(split[j])) {
-        map = new Map();
-        continue;
-      }
-
-      map.set(split[j], 1);
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
     }
+
+    set.add(s[right]);
+    longest = Math.max(longest, set.size);
   }
 
   return longest;
@@ -30,3 +25,8 @@ function lengthOfLongestSubstring(s: string): number {
 console.log(lengthOfLongestSubstring("abcabcbb"));
 console.log(lengthOfLongestSubstring("bbbbb"));
 console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring(" "));
+console.log(lengthOfLongestSubstring(""));
+console.log(lengthOfLongestSubstring("jbpnbwwd"));
+console.log(lengthOfLongestSubstring("aab"));
+console.log(lengthOfLongestSubstring("au"));
