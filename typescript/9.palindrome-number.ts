f934cb44be055ca5e012f5b571a2398d13ba3d6c
Benjamin Cofrancesco <benjamin@whitefoxboutique.com>
2026-01-23 10:25:50 +1100

    chore: added solution

--- typescript/9.palindrome-number.ts
+++ typescript/9.palindrome-number.ts
@@ -3,3 +3,23 @@
 function isPalindrome(x: number): boolean {
   return x === parseInt(x.toString().split("").reverse().join(""));
 }
+
+// constrant, you can't convert to string;
+
+function isPalindromeNoString(x: number): boolean {
+  if (x < 0 || (x % 10 === 0 && x !== 0)) return false;
+
+  let half = 0;
+  while (x > half) {
+    half = half * 10 + (x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  return x === half || x === Math.floor(half / 10);
+}
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(12321));
+
+console.log(isPalindromeNoString(121));
+console.log(isPalindromeNoString(12321));
